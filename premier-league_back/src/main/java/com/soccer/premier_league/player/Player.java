28747375b5554adaae61d7eb0f91a5e5a63fed6d
astgh <astgh.trdatyan.n@gmail.com>
2024-10-02 17:15:08 +0400

    first commit

---
+++ premier-league_back/src/main/java/com/soccer/premier_league/player/Player.java
@@ -0,0 +1,58 @@
+package com.soccer.premier_league.player;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "player_stats")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String nation;
+    private String pos;
+    private Integer age;
+    private Integer mp;
+    private Integer starts;
+    private Double min;
+    private Double gls;
+    private Double ast;
+    private Double pk;
+    private Double crdy;
+    private Double crdr;
+    private Double xg;
+    private Double xag;
+    private String team;
+
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", nation='" + nation + '\'' +
+                ", pos='" + pos + '\'' +
+                ", age=" + age +
+                ", mp=" + mp +
+                ", starts=" + starts +
+                ", min=" + min +
+                ", gls=" + gls +
+                ", ast=" + ast +
+                ", pk=" + pk +
+                ", crdy=" + crdy +
+                ", crdr=" + crdr +
+                ", xg=" + xg +
+                ", xag=" + xag +
+                ", team='" + team + '\'' +
+                '}';
+    }
+}
